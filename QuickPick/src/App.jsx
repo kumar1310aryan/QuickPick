657893ea6e46d25dfeebf8caf35af2cb89b568e5
kumar1310aryan <kumar1310aryan@gmail.com>
2024-10-18 08:20:27 +0530

    quick pick nav

--- QuickPick/src/App.jsx
+++ QuickPick/src/App.jsx
@@ -1,5 +1,32 @@
+import "./App.css";
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div id="main">
+        <nav>
+          <a href="/create">Add new Product</a>
+          <hr />
+          <h1>Category Filter</h1>
+          <ul>
+            <li>
+              <div></div> cat 1
+            </li>
+            <li>
+              <div></div> cat 2
+            </li>
+            <li>
+              <div></div> cat 3
+            </li>
+            <li>
+              <div></div> cat 4
+            </li>
+          </ul>
+        </nav>
+
+        <div id="home"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
